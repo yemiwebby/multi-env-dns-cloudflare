e123847216b81b98f0da2283f76b056d1cb41eea
yemiwebby <yemiwebby@gmail.com>
2025-07-13 09:20:24 +0100

    update terraform

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
+
 
 resource "cloudflare_record" "staging_api" {
   zone_id = var.cloudflare_zone_id
